b3e79270c5b64ef7d92d88828a301bc9afd9488a
Chaher Machhour <chaher.croissance.public@gmail.com>
2024-10-21 09:56:26 +0200

    Update

--- DeviseFlash/ContentView.swift
+++ DeviseFlash/ContentView.swift
@@ -8,17 +8,26 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State private var viewModel = DeviseViewModel()
+    
+    @State private var deviseSource = ["Dollar américain (USD)", "Euro (EUR)", "Yen japonais (JPY)", "Livre sterling (GBP)", "Franc suisse (CHF)"]
+    @State private var selectedDevise = "Dollar américain (USD)"
+    @State private var value: Decimal = 0
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        Form {
+            Picker("Devise Source", selection: $selectedDevise) {
+                ForEach(deviseSource, id: \.self) {devise in
+                    Text("\(devise)")
+                }
+            }
+            TextField("Entre un montant", value: $value, formatter: viewModel.currencyFormatter)
         }
-        .padding()
     }
 }
 
 #Preview {
     ContentView()
 }
+
